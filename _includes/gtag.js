26104ba38d5526546694e4d513895f605eb437b7
Michael Krzyzanski <michael.krzyzanski@logicalis.de>
2023-09-13 11:55:59 +0200

    Cookie Consent integration

---
+++ _includes/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SCE8J3S1D5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SCE8J3S1D5');
+</script>
